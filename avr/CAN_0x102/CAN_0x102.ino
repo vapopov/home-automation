c61be50fe9ddbc5e5a6a5e947d5980ca5cc18cc3
Vadym Popov <vpopov@ves.io>
2021-12-11 04:06:45 +0200

    working uart to faraday

--- avr/CAN_0x102/CAN_0x102.ino
+++ avr/CAN_0x102/CAN_0x102.ino
@@ -13,8 +13,10 @@
 MCP_CAN CAN0(10);     // Set CS to pin 10
 #define CAN0_INT 9    // Set INT to pin 9
 
-#define PWM_LED 3
+#define PWM_LED 3 // PD3, OC2B
 #define PIN_LED_RELAY 6
+
+#define PIN_WATER 7
 
 #define BUTTON_ZERO 4
 #define BUTTON_ONE 5
@@ -39,39 +41,6 @@ unsigned char rxBuf[8];
  * avrdude -c usbasp -p m328p
  */
 
-/***********************************************************************************
-If you send the following standard IDs below to an Arduino loaded with this sketch
-you will find that 0x102 and 0x105 will not get in.
-ID in Hex  -   Two Data Bytes!   -  Filter/Mask in HEX
-   0x100   + 0000 0000 0000 0000 =   0x01000000
-   0x101   + 0000 0000 0000 0000 =   0x01010000
-   0x102   + 0000 0000 0000 0000 =   0x01020000  This example will NOT be receiving this ID
-   0x103   + 0000 0000 0000 0000 =   0x01030000
-   0x104   + 0000 0000 0000 0000 =   0x01040000
-   0x105   + 0000 0000 0000 0000 =   0x01050000  This example will NOT be receiving this ID
-   0x106   + 0000 0000 0000 0000 =   0x01060000
-   0x107   + 0000 0000 0000 0000 =   0x01070000
-   This mask will check the filters against ID bit 8 and ID bits 3-0.   
-    MASK   + 0000 0000 0000 0000 =   0x010F0000
-   
-   If there is an explicit filter match to those bits, the message will be passed to the
-   receive buffer and the interrupt pin will be set.
-   This example will NOT be exclusive to ONLY the above frame IDs, for that a mask such
-   as the below would be used: 
-    MASK   + 0000 0000 0000 0000 = 0x07FF0000
-    
-   This mask will check the filters against all ID bits and the first data byte:
-    MASK   + 1111 1111 0000 0000 = 0x07FFFF00
-   If you use this mask and do not touch the filters below, you will find that your first
-   data byte must be 0x00 for the message to enter the receive buffer.
-   
-   At the moment, to disable a filter or mask, copy the value of a used filter or mask.
-   
-   Data bytes are ONLY checked when the MCP2515 is in 'MCP_STDEXT' mode via the begin
-   function, otherwise ('MCP_STD') only the ID is checked.
-***********************************************************************************/
-
-
 OneButton btn0 = OneButton(
   BUTTON_ZERO, // Input pin for the button
   true,        // Button is active LOW
@@ -88,10 +57,7 @@ OneButton btn1 = OneButton(
 void setup()
 {
   Serial.begin(9600, SERIAL_8N1);
-
-  // Initialize MCP2515 running at 8MHz with a baudrate of 125kb/s and the masks and filters disabled.
-  //  if(CAN0.begin(MCP_STD, CAN_125KBPS, MCP_16MHZ) == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
-  //  else Serial.println("Error Initializing MCP2515...");
+  Serial.setTimeout(2000);
 
   CAN0.begin(MCP_STD, CAN_125KBPS, MCP_16MHZ);
 
@@ -110,6 +76,7 @@ void setup()
   pinMode(CAN0_INT, INPUT);
   pinMode(PWM_LED, OUTPUT);
   pinMode(PIN_LED_RELAY, OUTPUT);
+  pinMode(PIN_WATER, OUTPUT);
 
 
   // ------------------------------ Set Timers -------------------------------------
@@ -120,6 +87,23 @@ void setup()
   TCNT1 = 0;              //Reset Timer 1 value to 0
   // TIMSK1 |= B00000010; //Set OCIE1A to 1 so we enable compare match A 
   OCR1A = 312;            //Finally we set compare register A to this value ~ 5ms
+  
+  // +++++++++ timer 2 ++++++++++++++++++++
+  /*set fast PWM mode with non-inverted output*/
+  TCCR2A = (0 << WGM02) | (1 << WGM01) | (1 << WGM00);
+  // Normal port operation, OC2A disconnected.
+  TCCR2A |= (0 << COM2A1) | (0 << COM2A0); 
+  // Normal port operation, OC2B disconnected
+  TCCR2A |= (0 << COM2B1) | (0 << COM2B0); 
+  // B Register +++++++++++++++++++++++++++
+  TCCR2B = (0 << CS22) | (0 << CS21) | (1 << CS20); // no prescaller
+
+  // Пины D3 и D11 - 62.5 кГц
+//  TCCR2B = 0b00000001;  // x1
+//  TCCR2A = 0b00000011;  // fast pwm
+//  // Пины D3 и D11 - 31.4 кГц
+//  TCCR2B = 0b00000001;  // x1
+//  TCCR2A = 0b00000001;  // phase correct  
   sei();
 
   // -------------------------------------------------------------------------------
@@ -234,18 +218,21 @@ void checkUPS()
 {
   byte const req[] = {0x55, 0x01, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x56};
   Serial.write(req, 13);
-  Serial.readBytes(upsResp, 13); // since tx connected to rx, we have to read same message.
-  Serial.readBytes(upsResp, 16);
+  if (Serial.available() > 0) {
+      //Serial.readBytes(upsResp, 13); // since tx connected to rx, we have to read same message.
+      Serial.find(0x56);
+      Serial.readBytes(upsResp, 16);
+  }
 
   byte crc = 0;
   for(uint8_t i = 0; i < 15; i++) {
     crc += upsResp[i];
   }
 
-  if (crc == upsResp[15]) {
+  //if (crc == upsResp[15]) {
     // Set voltage data.
     pong[2] = upsResp[2];
-    pong[3] = upsResp[2];
+    pong[3] = upsResp[3];
     // Set current data.
     pong[4] = upsResp[4];
     pong[5] = upsResp[5];
@@ -270,15 +257,16 @@ void checkUPS()
     } else if (upsResp[8] == 2) {
       pong[6] |= AKB_UNDER_CUR;
     }
-  } else {
-    pong[2] = 0;
-    pong[3] = 0;
-    pong[4] = 0;
-    pong[5] = 0;
-    pong[6] = 0;
-  }
+//  } else {
+//    pong[2] = 0;
+//    pong[3] = 0;
+//    pong[4] = 0;
+//    pong[5] = 0;
+//    pong[6] = 0;
+//  }
 }
 
+bool checkUPSEnabled = false;
 
 // ----------------------------------------- Main Loop --------------------------------------------
 void loop()
@@ -290,6 +278,15 @@ void loop()
     CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
     // (rxId & DEVICE_ADDR) == DEVICE_ADDR)
     if((rxId & 0x40000000) == 0x40000000) {
+      if (checkUPSEnabled) {
+        checkUPS();
+      } else {
+        pong[2] = 0;
+        pong[3] = 0;
+        pong[4] = 0;
+        pong[5] = 0;
+        pong[6] = 0;
+      }
       // Determine if message is a remote request frame.
        CAN0.sendMsgBuf(DEVICE_ADDR, 0, 7,  pong);
        pong[1]++;
@@ -313,8 +310,19 @@ void loop()
           OCR1A = rxBuf[3] << 8 | rxBuf[4]; // step
           pwmVal = pwmValFrom;
           TIMSK1 |= B00000010;              //Set OCIE1A enable interrupt
-       } else if (rxBuf[0] = 0x6) {
-          checkUPS();
+       } else if(rxBuf[0] == 0x6) { /// digital writing for output, need for closing water 
+          pwmValFrom = rxBuf[1];            // from
+          if (rxBuf[1] == 0) {
+            digitalWrite(PIN_WATER, false);
+          } else {
+            digitalWrite(PIN_WATER, true);
+          }    
+       } else if (rxBuf[0] = 0x1a) {
+          if (rxBuf[1] > 0) {
+            checkUPSEnabled = true;
+          } else {
+            checkUPSEnabled = false;
+          }
        }
 
        if(pwmVal == 0) {
